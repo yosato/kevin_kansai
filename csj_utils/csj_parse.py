78672ae6fb1654fe157a78a9091ef8bbab57db8d
Yo Sato <yosato16@gmail.com>
2018-11-24 17:37:17 +0000

    csj xml parser

--- csj_utils/csj_parse.py
+++ csj_utils/csj_parse.py
@@ -2,27 +2,35 @@ from xml.etree import ElementTree as ET
 import os,sys,json
 from collections import defaultdict
 
-def main0(XmlFP,OutputFP,TgtNodes='all'):
+def main0(XmlFP,OutputFP):
+    '''
+    this returns the dict with sentence IDs as its keys and their corresponding LUWs as its values
+    An LUW is an 'Element' object of ElementTree API of Python standard lib, and contains SUWs as its children, so that's all you'd need.
+    Refer to https://docs.python.org/3/library/xml.etree.elementtree.html for details of this object
+    '''
     ETree=ET.parse(XmlFP)
+    FndEls=defaultdict(list)
     with open(OutputFP,'wt') as FSw:
         InitNode=ETree.getroot()
-        Nodes=find_nodes_in_tree(InitNode,defaultdict(list),TgtNodes=TgtNodes)
-        for (Node,Attrib) in Nodes:
-            sys.stdout.write('\n'.join([Node,repr(Attrib)])+'\n\n')
-            FSw.write(json.dumps([Node,Attrib])+'\n')
+        Nodes=find_nodes_in_tree(InitNode,[],TgtNodes=['LUW'],ParentToo=True)
+        for (HitLUW,Parent) in Nodes:
+            SentID=int(Parent.attrib['IPUID'])
+            FndEls[SentID].append(HitLUW)
 
-def find_nodes_in_tree(ParentNode,ResNodes,TgtNodes='all'):
+
+def find_nodes_in_tree(ParentNode,ResNodes,TgtNodes='all',ParentToo=True):
     assert TgtNodes=='all' or (isinstance(TgtNodes,list) and len(TgtNodes)==len(set(TgtNodes)))
     
     for ChildNode in ParentNode:
         if TgtNodes=='all' or ChildNode.tag in TgtNodes:
-            ResNodes[ChildNode.tag].append(ChildNode.attrib)
+            FndEl=(ChildNode,ParentNode) if ParentToo else ChildNode
+            ResNodes.append(FndEl)
             #TgtNodes.remove(ChildNode)
         Children=ChildNode.getchildren()
-        if all(TgtNode in [Child.tag for Child in Children] for TgtNode in TgtNodes):
+        if Children:
             ResNodes=find_nodes_in_tree(ChildNode,ResNodes,TgtNodes=TgtNodes)
-        elif any(TgtNode in [Child.tag for Child in Children] for TgtNode in TgtNodes):
-            sys.stderr.write('something funny')
+#        elif any(TgtNode in [Child.tag for Child in Children] for TgtNode in TgtNodes):
+#            sys.stderr.write('something funny')
             
 
     return ResNodes
@@ -32,13 +40,13 @@ def main():
     Psr=argparse.ArgumentParser()
     Psr.add_argument('input_fp')
     Psr.add_argument('--output-fp')
-    Psr.add_argument('--target-nodes',type=list,default=['LUW','SUW'])
+
     Args=Psr.parse_args()
     if not Args.input_fp.endswith('.xml'):
         sys.exit('input file must end with xml extension')
     if Args.output_fp is None:
         Args.output_fp=(''.join(Args.input_fp.split('.')[:-1]+['.parse']))
-    main0(Args.input_fp,Args.output_fp,TgtNodes=Args.target_nodes)
+    main0(Args.input_fp,Args.output_fp)
 
 
 if __name__ == '__main__':
