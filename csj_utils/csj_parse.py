398c8c1114fb77e0ea901a087c53836a197349b6
Yo Sato <yosato16@gmail.com>
2018-12-27 08:29:31 +0000

    hello

--- csj_utils/csj_parse.py
+++ csj_utils/csj_parse.py
@@ -1,36 +1,54 @@
 from xml.etree import ElementTree as ET
 import os,sys
-from collections import defaultdict
+from collections import defaultdict,OrderedDict
 
-def main0(XmlFP,OutFP=None):
-    SentsLUWs=get_luws(XmlFP)
+def main0(XmlFP):
+    SentsLUWs=get_luws(XmlFP,IPUMergeCnt=10)
     WantedFt='PlainOrthographicTranscription'
-    Out=sys.stdout if OutFP is None else open(OutFP,'wt')
-    for LUWs in SentsLUWs.values():
-        for LUW in LUWs:
-            if 'LUWPOS' in LUW.attrib.keys() and LUW.attrib['LUWPOS']=='名詞':
-                SUWs=[Child for Child in LUW.getchildren() if Child.tag=='SUW']
-                SUWsStr=' '.join([SUW.attrib[WantedFt] for SUW in SUWs])
-                sys.stdout.write(SUWsStr+'\n')
-    if OutFP:
-        Out.close()
+ #   Out=sys.stdout if OutFP is None else open(OutFP,'wt')
+    for LUWsPerIPU in SentsLUWs.values():
+        for LUW in LUWsPerIPU:
+            SUWs=[Child for Child in LUW.getchildren() if Child.tag=='SUW']
+            SUWsStr=' '.join([SUW.attrib[WantedFt] for SUW in SUWs])
+            #if 'LUWPOS' in LUW.attrib.keys() and LUW.attrib['LUWPOS']=='名詞':
+                
+            
+            
+            sys.stdout.write(SUWsStr+'\n')
+    #if OutFP:
+     #   Out.close()
 
-def get_luws(XmlFP):
+def get_luws(XmlFP,IPUMergeCnt=0):
     '''
     this is the main func, which returns the dict with sentence IDs as its keys and their corresponding LUWs as its values
     An LUW is an 'Element' object of ElementTree API of Python standard lib, and contains SUWs as its children, so that's all you'd need.
     Refer to https://docs.python.org/3/library/xml.etree.elementtree.html for details of this object
     '''
     ETree=ET.parse(XmlFP)
-    FndLUWs=defaultdict(list)
+    FndLUWs=OrderedDict()
     InitNode=ETree.getroot()
-    Nodes=find_nodes_in_tree(InitNode,[],TgtNodes=['LUW'],ParentToo=True)
-    for (HitLUW,Parent) in Nodes:
-        SentID=int(Parent.attrib['IPUID'])
-        FndLUWs[SentID].append(HitLUW)
+    FndLUWs=OrderedDict()
+    LUWsPerIPU=[];IPUIDs=[]
+    for IPU in generate_nodes(InitNode,'IPU'):
+        #LUWsPerIPU=[]
+        IPUID=IPU.attrib['IPUID']
+        Nodes=find_nodes_in_tree(IPU,[],TgtNodes=['LUW'],ParentToo=True)
+        for (HitLUW,_) in Nodes:
+            if 'LUWPOS' in HitLUW.attrib.keys() and HitLUW.attrib['LUWPOS']!='感動詞':
+                LUWsPerIPU.append(HitLUW)
+        if len(LUWsPerIPU)>=IPUMergeCnt:
+            FndLUWs[tuple(IPUIDs+[IPUID])]=LUWsPerIPU
+            LUWsPerIPU=[];IPUIDs=[]
+        else:
+            IPUIDs.append(IPUID)
+    if IPUMergeCnt and LUWsPerIPU:
+        FndLUWs[tuple(IPUIDs)]=LUWsPerIPU
+    return FndLUWs
 
-    return FndLUWs    
-
+def generate_nodes(ParentNode,NodeName):
+    for ChildNode in ParentNode:
+        if ChildNode.tag==NodeName:
+            yield ChildNode
 
 def find_nodes_in_tree(ParentNode,ResNodes,TgtNodes='all',ParentToo=True):
     assert TgtNodes=='all' or (isinstance(TgtNodes,list) and len(TgtNodes)==len(set(TgtNodes)))
@@ -58,7 +76,7 @@ def main():
     Args=Psr.parse_args()
     if not Args.input_fp.endswith('.xml'):
         sys.exit('input file must end with xml extension')
-    main0(Args.input_fp,Args.output_fp)
+    main0(Args.input_fp)
 
 
 if __name__ == '__main__':
