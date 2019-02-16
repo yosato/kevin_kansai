642f9368e502b205174546946123e4e699fc1cc8
Yo Sato <yosato16@gmail.com>
2019-02-16 08:52:37 +0000

    sent based segmentation for csj

--- csj_utils/csj_parse.py
+++ csj_utils/csj_parse.py
@@ -2,53 +2,63 @@ from xml.etree import ElementTree as ET
 import os,sys
 from collections import defaultdict,OrderedDict
 
-def main0(XmlFP):
-    SentsLUWs=get_luws(XmlFP,IPUMergeCnt=10)
-    WantedFt='PlainOrthographicTranscription'
- #   Out=sys.stdout if OutFP is None else open(OutFP,'wt')
-    for LUWsPerIPU in SentsLUWs.values():
-        for LUW in LUWsPerIPU:
-            SUWs=[Child for Child in LUW.getchildren() if Child.tag=='SUW']
-            SUWsStr=' '.join([SUW.attrib[WantedFt] for SUW in SUWs])
+def main0(XmlFP,WantedFts=[]):
+    for LUWsPerUnit in generate_grouped_luws(XmlFP):
+        for LUW in LUWsPerUnit:
+            SUWs=get_suws(LUW)
+            SUWsStr=' '.join([(SUW.attrib['Dep_BunsetsuUnitID'] if 'Dep_BunsetsuUnitID' in SUW.attrib.keys() else '')+' '+SUW.attrib['PlainOrthographicTranscription'] for SUW in SUWs])
+            if WantedFts:
+                FtsStr='\t'
+                for (Ind,SUW) in enumerate(SUWs):
+                    for WantedFt in WantedFts:
+                        FtsStr+=' '+WantedFt+':'+SUW.attrib[WantedFt] if WantedFt in SUW.attrib.keys() else ''
             #if 'LUWPOS' in LUW.attrib.keys() and LUW.attrib['LUWPOS']=='名詞':
-                
-            
-            
-            sys.stdout.write(SUWsStr+'\n')
+            sys.stdout.write(SUWsStr+FtsStr+'\n')
     #if OutFP:
      #   Out.close()
 
-def get_luws(XmlFP,IPUMergeCnt=0):
+def get_repeated_list(El,Times):
+    L=[]
+    for i in range(Times):
+        L.append(El)
+    return L
+def get_suws(LUW):
+    return [Child.attrib['SUW'] for Child in LUW.get_children()]
+def get_next_suwfeats(LUW,FtNames):
+    Fts=get_repeated_list(None,len(FtNames))
+    for SUW in LUW.get_suws(LUW):
+        for (Ind,FtName) in enumerate(FtNames):
+            if FtName in SUW.attrib.keys() and Fts[Ind] is None:
+                Fts[Ind]=SUW.attrib[FtName]
+    return Fts
+
+
+def generate_grouped_luws(XmlFP,Unit='Sentence'):
     '''
     this is the main func, which returns the dict with sentence IDs as its keys and their corresponding LUWs as its values
     An LUW is an 'Element' object of ElementTree API of Python standard lib, and contains SUWs as its children, so that's all you'd need.
     Refer to https://docs.python.org/3/library/xml.etree.elementtree.html for details of this object
     '''
+    assert(Unit in ['IPU','Sentence'])
     ETree=ET.parse(XmlFP)
-    FndLUWs=OrderedDict()
+    FndLUWs=[]
     InitNode=ETree.getroot()
     FndLUWs=OrderedDict()
-    LUWsPerIPU=[];IPUIDs=[]
-    for IPU in generate_nodes(InitNode,'IPU'):
-        #LUWsPerIPU=[]
-        IPUID=IPU.attrib['IPUID']
-        Nodes=find_nodes_in_tree(IPU,[],TgtNodes=['LUW'],ParentToo=True)
-        for (HitLUW,_) in Nodes:
-            if 'LUWPOS' in HitLUW.attrib.keys() and HitLUW.attrib['LUWPOS']!='感動詞':
-                LUWsPerIPU.append(HitLUW)
-        if len(LUWsPerIPU)>=IPUMergeCnt:
-            FndLUWs[tuple(IPUIDs+[IPUID])]=LUWsPerIPU
-            LUWsPerIPU=[];IPUIDs=[]
+    LUWsPerUnit=[];PrvUnitID=0
+    for LUW in generate_nodes(InitNode,'LUW'):
+        CurUnitID,CurModID=get_next_suwfeats(LUW,['Dep_BunsetsuUnitID','Dep_ModifieeBunsetsuUnitID'])
+        if CurUnitID is not None:
+            if CurUnitID<PrvUnitID:
+                yield FndLUWs
         else:
-            IPUIDs.append(IPUID)
-    if IPUMergeCnt and LUWsPerIPU:
-        FndLUWs[tuple(IPUIDs)]=LUWsPerIPU
-    return FndLUWs
+            FndLUWs.append(LUW)
 
 def generate_nodes(ParentNode,NodeName):
     for ChildNode in ParentNode:
         if ChildNode.tag==NodeName:
             yield ChildNode
+        else:
+            generate_nodes(ChildNode,NodeName)
 
 def find_nodes_in_tree(ParentNode,ResNodes,TgtNodes='all',ParentToo=True):
     assert TgtNodes=='all' or (isinstance(TgtNodes,list) and len(TgtNodes)==len(set(TgtNodes)))
@@ -72,11 +82,12 @@ def main():
     Psr=argparse.ArgumentParser()
     Psr.add_argument('input_fp')
     Psr.add_argument('--output-fp')
+    Psr.add_argument('--wanted-fts',nargs='+')
 
     Args=Psr.parse_args()
     if not Args.input_fp.endswith('.xml'):
         sys.exit('input file must end with xml extension')
-    main0(Args.input_fp)
+    main0(Args.input_fp,WantedFts=Args.wanted_fts)
 
 
 if __name__ == '__main__':
