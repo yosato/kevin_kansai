84b0cd56541a98911df5e4dfaf9e5a07f526400e
Kevin Heffernan <kevinh@zeus.eonet.ne.jp>
2020-11-23 17:00:11 +0900

    added r command file

--- hatsuonbin_project/R_data/r_script.R
+++ hatsuonbin_project/R_data/r_script.R
@@ -52,5 +52,49 @@ summary(model4d)
 #model 4a: -3168.7
 #model 4b: -3231.0, wpbp not significant
 #model 4c: -3073.4 *** best model
-#model 4d: model failed to converge
+#model 4d: -3106.9
+#result: keep mpfp in the model
+
+###the opaque data###
+#model 5; test age, gender, and speech style; result: only speech style is significant
+model5 <- glmer(onbin ~ age + gender + speech_style + (1|speaker) + (1|word), data = onbin_opaque, family = binomial, control = glmerControl(optimizer = "bobyqa"), nAGQ = 1)
+summary(model5)
+
+#model 6; add in adjacent, f_pos; result both adjacent and f_pos significant
+model6 <- glmer(onbin ~ speech_style + adjacent + f_pos + (1|speaker) + (1|word), data = onbin_opaque, family = binomial, control = glmerControl(optimizer = "bobyqa"), nAGQ = 1)
+summary(model6)
+
+#models 7a, 7b, 7c, 7d; test wFreq, mFreq, wpFreq, and mpFreq separately as models 7a 7b 7c 7d respectively
+model7a <- glmer(onbin ~ speech_style + adjacent + f_pos + wFreq + (1|speaker) + (1|word), data = onbin_opaque, family = binomial, control = glmerControl(optimizer = "bobyqa"), nAGQ = 1)
+summary(model7a)
+model7b <- glmer(onbin ~ speech_style + adjacent + f_pos + mFreq + (1|speaker) + (1|word), data = onbin_opaque, family = binomial, control = glmerControl(optimizer = "bobyqa"), nAGQ = 1)
+summary(model7b)
+model7c <- glmer(onbin ~ speech_style + adjacent + f_pos + wpFreq + (1|speaker) + (1|word), data = onbin_opaque, family = binomial, control = glmerControl(optimizer = "bobyqa"), nAGQ = 1)
+summary(model7c)
+model7d <- glmer(onbin ~ speech_style + adjacent + f_pos + mpFreq + (1|speaker) + (1|word), data = onbin_opaque, family = binomial, control = glmerControl(optimizer = "bobyqa"), nAGQ = 1)
+summary(model7d)
+
+#log likelihood results for the four models (closer to zero = better result)
+#model 7a: -433.4
+#model 7b: -428.4
+#model 7c: -365.4
+#model 7d: -356.9 *** best model
+#result: keep mpFreq in the model
+
+#models 8a, 8b, 8c, 8d; test in wpfp, wpbp, mpfp, and mpbp separately as models 8a 8b 8c 8d respectively
+model8a <- glmer(onbin ~ speech_style + adjacent + f_pos + mpFreq + wpfp + (1|speaker) + (1|word), data = onbin_opaque, family = binomial, control = glmerControl(optimizer = "bobyqa"), nAGQ = 1)
+summary(model8a)
+model8b <- glmer(onbin ~ speech_style + adjacent + f_pos + mpFreq + wpbp + (1|speaker) + (1|word), data = onbin_opaque, family = binomial, control = glmerControl(optimizer = "bobyqa"), nAGQ = 1)
+summary(model8b)
+model8c <- glmer(onbin ~ speech_style + adjacent + f_pos + mpFreq + mpfp + (1|speaker) + (1|word), data = onbin_opaque, family = binomial, control = glmerControl(optimizer = "bobyqa"), nAGQ = 1)
+summary(model8c)
+model8d <- glmer(onbin ~ speech_style + adjacent + f_pos + mpFreq + mpbp + (1|speaker) + (1|word), data = onbin_opaque, family = binomial, control = glmerControl(optimizer = "bobyqa"), nAGQ = 1)
+summary(model8d)
+
+
+#log likelihood results for the four models (closer to zero = better result)
+#model 8a: -211.2, wpfp sign.
+#model 8b: -277.2, wpbp not sign.
+#model 8c: -105.0, mpfp sign., *** best model
+#model 8d: -352.9, mpbp not sign.
 #result: keep mpfp in the model
